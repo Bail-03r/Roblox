6d16e079845096bb1747e0d1912a13f52c373156
Bail-03r <63909387+Bail-03r@users.noreply.github.com>
2021-12-04 19:51:43 +0300

    Create script.lua

---
+++ [Convert]/script.lua
@@ -0,0 +1,19 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))() local Window = Library.CreateLib("bqhax|Convert|", "DarkTheme") local Tab = Window:NewTab("Reanimate") local Section = Tab:NewSection("Choose")
+
+Section:NewButton("Click Here", "Reanimate Button", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bail-03r/Roblox/main/%5BConvert%5D/Reanim.lua"))()
+end)
+
+local Tab = Window:NewTab("Scripts") local Section = Tab:NewSection("Choose")
+
+Section:NewButton("JoJo Stand", "Script Button", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bail-03r/Roblox/main/%5BConvert%5D/Ora.lua"))()
+end)
+
+Section:NewButton("Parkour", "Script Button", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bail-03r/Roblox/main/%5BConvert%5D/parkour.lua"))()
+end)
+
+Section:NewButton("caramelidance", "Reanimate Button", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bail-03r/Roblox/main/%5BConvert%5D/caramelidance.lua"))()
+end)
